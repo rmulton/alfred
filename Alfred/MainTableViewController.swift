dff1477de993d8eed5ceefbadfe56fe4de057dd3
Robin <rmulton@gmail.com>
2017-02-17 01:48:26 +0100

    Added some economic figures to the data downloaded for the example.

--- Alfred/MainTableViewController.swift
+++ Alfred/MainTableViewController.swift
@@ -20,7 +20,7 @@ class MainTableViewController: UITableViewController, NSFetchedResultsController
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateCategory()
+        updateCategories()
         fetchCategories()
     }
 
@@ -170,18 +170,17 @@ class MainTableViewController: UITableViewController, NSFetchedResultsController
     
     // Mark: - Downloading data from news api
     
-    func downloadFromSource(category: Category, urlString: String, resetDatabase: Bool){
+    func downloadFromSource(category: Category, urlString: String){
         
         let currentNewsURL = URL(string: urlString)
         
         Alamofire.request(currentNewsURL!).responseJSON { (response) in
             
-            if resetDatabase{
-                self.resetDatabase() // must be here because of asynchronous thread
-            }
             
             let result = response.result
             if let dict = result.value as? Dictionary<String, Any>{
+                
+                // News api
                 let source = dict["source"] as? String
                 if let articles = dict["articles"] as? [Dictionary<String, Any>]{
                     let detailSection = DetailSection(context: context)
@@ -197,103 +196,71 @@ class MainTableViewController: UITableViewController, NSFetchedResultsController
                                 detail.descript = descript
                                 detail.title = title
                                 detail.toDetailSection = detailSection
-                            
                         }
-                        
                     }
                 }
+                
+                //  Quandl api
+                if let dataset = dict["dataset"] as? Dictionary<String, Any>{
+                    
+                    let detailSection = DetailSection(context: context)
+                    detailSection.title = "France" // to change
+                    detailSection.toCategory = category
+                    
+                    if let name = dataset["name"] as? String, let data = dataset["data"] as? [[Any]]{
+                        let detail = Detail(context: context)
+                        detail.title = name
+                        detail.source = "ODA"
+                        detail.descript = "\(data[5][1])"
+                        detail.toDetailSection = detailSection
+                    }
+                    
+                }
+                
+                
+                
+                
             }
         }
 
     }
     
-    func updateCategory(){
-        
+    func loadNewsCategory(){
+        let urlStrings = [THE_ECONOMIST, HACKER_NEWS]
         let category = Category(context: context)
         category.title = "Today's headlines"
         category.descript = "I summed it up for you !"
+        category.image = #imageLiteral(resourceName: "news")
+        for urlString in urlStrings{
+            downloadFromSource(category: category, urlString: urlString)
+        }
+    }
+    
+    func loadEconomicDataCategory(){
+        
+        let category = Category(context: context)
+        category.title = "Economic figures"
+        category.descript = "Important figures to follow"
         category.image = #imageLiteral(resourceName: "economics")
         
-        downloadFromSource(category: category, urlString: THE_ECONOMIST, resetDatabase: false) // reset mis à false pour tenter dans viewWillAppear
-        downloadFromSource(category: category, urlString: HACKER_NEWS, resetDatabase: false)
+        let urlStrings = [
+            "https://www.quandl.com/api/v3/datasets/ODA/FRA_LUR.json?api_key=AMPx4_PVr83RgqPR6uUy",
+            "https://www.quandl.com/api/v3/datasets/ODA/FRA_PCPIPCH.json?api_key=AMPx4_PVr83RgqPR6uUy",
+            "https://www.quandl.com/api/v3/datasets/ODA/FRA_TMG_RPCH.json?api_key=AMPx4_PVr83RgqPR6uUy",
+            "https://www.quandl.com/api/v3/datasets/ODA/FRA_TXG_RPCH.json?api_key=AMPx4_PVr83RgqPR6uUy"
+        ]
         
+        for urlString in urlStrings{
+            downloadFromSource(category: category, urlString: urlString)
+        }
+    }
+    
+    func updateCategories(){
+        
+        loadNewsCategory()
+        loadEconomicDataCategory()
         ad.saveContext()
     
     }
     
-    
-    
-    
-    // Mark: - Used during developement
-    
-    /* Not used anymore
-    func generateSampleData(){
-        
-
-        // Sample data
-        
-            // Categories
-        let category0 = Category(context: context)
-        category0.title = "Press review"
-        category0.descript = "I summed up the headlines for you :)"
-        category0.image = #imageLiteral(resourceName: "news")
-        
-        let category1 = Category(context: context)
-        category1.title = "The world today"
-        category1.descript = "Facts and figures to know"
-        category1.image = #imageLiteral(resourceName: "economics")
-        
-        let category2 = Category(context: context)
-        category2.title = "Review bookmarked articles"
-        category2.descript = "3 articles a day keep the mess away"
-        category2.image = #imageLiteral(resourceName: "write")
-
-        // Detail sections
-        let detailSection00 = DetailSection(context: context)
-        detailSection00.title = "Europe"
-        detailSection00.toCategory = category0
-        
-        let detailSection01 = DetailSection(context: context)
-        detailSection01.title = "USA"
-        detailSection01.toCategory = category0
-        
-        let detailSection10 = DetailSection(context: context)
-        detailSection10.title = "Economic figures"
-        detailSection10.toCategory = category1
-        
-        // Details
-        let detail00 = Detail(context: context)
-        detail00.title = "François Bayrou se présente à l'élection présidentielle"
-        detail00.source = "Le Monde"
-        detail00.descript = "Pour la quatrième fois, le représentant du centre libre se présente à l'élection reine de la 5eme république"
-        detail00.toDetailSection = detailSection00
-        
-        let detail01 = Detail(context: context)
-        detail01.title = "Angela Merkel annonce un plan de relance budgétaire en Allemagne"
-        detail01.source = "Les Échos"
-        detail01.descript = "Le plan de relance se compose d'une injection de 50 milliards dans l'économie industrielle et d'une dotation d'un milliard à l'éducation."
-        detail01.toDetailSection = detailSection00
-        
-        let detail02 = Detail(context: context)
-        detail02.title = "Le président Trump multiplie les mésures contre l'imigration"
-        detail02.source = "AFP"
-        detail02.descript = "Le président a décidé d'interdire l'entrée dans le pays aux ressortissants de 7 pays musulmans."
-        detail02.toDetailSection = detailSection01
-        
-        let detail10 = Detail(context: context)
-        detail10.title = "Inflation UE : 3.5%"
-        detail10.source = "Yahoo finance"
-        detail10.descript = "USA 4% | JAP 2% || 2016 | USA 5% | UE 1% | JAP 4%"
-        detail10.toDetailSection = detailSection10
-        
-        let detail11 = Detail(context: context)
-        detail11.title = "Croissance UE : 1.5%"
-        detail11.source = "Yahoo finance"
-        detail11.descript = "USA 3% | JAP 1% || 2016 : USA 0.6% | UE 1% | JAP 4%"
-        detail11.toDetailSection = detailSection10
-        
-        ad.saveContext()
-    }
-    */
-
 }
